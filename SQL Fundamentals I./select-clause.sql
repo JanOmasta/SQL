01012f0e434174bc3c80d7dca0ff05c67469c8e9
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-06 18:23:19 +0100

    Update select-clause.sql

--- SQL Fundamentals I./select-clause.sql
+++ SQL Fundamentals I./select-clause.sql
@@ -1,14 +1,1 @@
--- Select a database with the keyword USE and the database name.
-USE sql_store;
-
-SELECT 
-	customer_id,		-- Select a column name. 
-    first_name, 
-    last_name, 
-    city, 
-    state, 
-    points, 
-    (points + 10) * 100 AS discount_factor		-- Clear descriptive name of a column using AS keyword.
-FROM customers									-- Select a table name.
-WHERE state = 'FL'								-- To filter the results using keyword WHERE.
-ORDER BY last_name 								-- To sort the results alpabetically.
+oo							-- To sort the results alpabetically.
