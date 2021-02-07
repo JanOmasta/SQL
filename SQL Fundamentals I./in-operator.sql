64122252924606d7b0070b04e85a9772ff2f8924
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-07 14:24:16 +0100

    Add files via upload

---
+++ SQL Fundamentals I./in-operator.sql
@@ -0,0 +1,7 @@
+USE sql_store;
+
+SELECT * 
+FROM customers
+-- WHERE state = 'VA' OR state = 'GA' OR state = 'FL'
+WHERE state IN ('VA', 'GA', 'FL')							-- Equivalent to the code on line 5.
+-- WHERE state NOT IN ('VA', 'GA', 'FL') 					-- Returns customers outside those 3 states.
