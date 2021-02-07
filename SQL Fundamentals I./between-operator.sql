9ce753e6c78179aa2e59a84619bc6d764ca947ae
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-07 16:29:23 +0100

    Update between-operator.sql

--- SQL Fundamentals I./between-operator.sql
+++ SQL Fundamentals I./between-operator.sql
@@ -3,5 +3,5 @@ USE sql_store;
 SELECT *
 FROM customers
 -- WHERE points >= 1000 AND points <= 3000
-WHERE points BETWEEN 1000 AND 3000								-- Equivalent to the code on line 5.
+WHERE points BETWEEN 1000 AND 3000                              -- Equivalent to the code on line 5.
 -- WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
