1c07a56a8adaba63a1123c5b31aa31f7e8c3efaa
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-07 13:35:31 +0100

    Update where-clause.sql

--- SQL Fundamentals I./where-clause.sql
+++ SQL Fundamentals I./where-clause.sql
@@ -2,9 +2,9 @@ USE sql_store;
 
 SELECT *
 FROM customers
-WHERE points > 3000					        -- If this condition is true, the result is shown.
--- WHERE state = 'VA'				        -- Gives all customers from Virginia.
--- WHERE state != 'VA'				      -- Gives all customers from states except Virginia. 
+WHERE points > 3000					     -- If this condition is true, the result is shown.
+-- WHERE state = 'VA'				      -- Gives all customers from Virginia.
+-- WHERE state != 'VA'				     -- Gives all customers from states except Virginia. 
 -- WHERE birth_date > '1990-01-01'	-- Gives all customers born after January 1, 1990.
 
 /*
