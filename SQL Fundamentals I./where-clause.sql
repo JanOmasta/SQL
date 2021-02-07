23949b95b73d368d7e2a3a7b430937f45db58f8a
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-07 13:33:50 +0100

    Add files via upload

---
+++ SQL Fundamentals I./where-clause.sql
@@ -0,0 +1,18 @@
+USE sql_store;
+
+SELECT *
+FROM customers
+WHERE points > 3000					-- If this condition is true, the result is shown.
+-- WHERE state = 'VA'				-- Gives all customers from Virginia.
+-- WHERE state != 'VA'				-- Gives all customers from states except Virginia. 
+-- WHERE birth_date > '1990-01-01'	-- Gives all customers born after January 1, 1990.
+
+/*
+Operators:
+>
+>=
+<
+<=
+=
+!= or equivalent is <>
+*/ 
