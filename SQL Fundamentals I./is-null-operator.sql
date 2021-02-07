36a32a68b4502c92aa488ea0e6d7c2b1510ef1ff
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-07 17:58:03 +0100

    Update is-null-operator.sql

--- SQL Fundamentals I./is-null-operator.sql
+++ SQL Fundamentals I./is-null-operator.sql
@@ -2,5 +2,5 @@ USE sql_store;
 
 SELECT *
 FROM customers
-WHERE phone IS NULL					-- Returns all customers with missing phone number.
--- WHERE phone IS NOT NULL			-- Returns all customers with stated phone number.
+WHERE phone IS NULL             -- Returns all customers with missing phone number.
+-- WHERE phone IS NOT NULL      -- Returns all customers with stated phone number.
