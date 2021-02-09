b03f02029a9622d46e7aaa239698b6a874b084e1
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-09 16:21:12 +0100

    Update inner-joins.sql

--- SQL Fundamentals II./inner-joins.sql
+++ SQL Fundamentals II./inner-joins.sql
@@ -1,13 +1,13 @@
 USE sql_store;
 
-SELECT order_id, orders.customer_id, first_name, last_name		-- Select customer_id from orders table (because they are in both).
+SELECT order_id, orders.customer_id, first_name, last_name			-- Select customer_id from orders table (because they are in both).
 FROM orders
 INNER JOIN customers 
-		  ON orders.customer_id = customers.customer_id	 		-- I want to join orders table with customers table. Joins these tables and return result. 
+	  ON orders.customer_id = customers.customer_id				-- I want to join orders table with customers table. Joins these tables and return result. 
           
 /*          
-SELECT order_id, o.customer_id, first_name, last_name			-- Substitution of orders as o and customers as c.
+SELECT order_id, o.customer_id, first_name, last_name				-- Substitution of orders as o and customers as c.
 FROM orders o
 INNER JOIN customers c 
-		  ON o.customer_id = c.customer_id
+	  ON o.customer_id = c.customer_id
 */
