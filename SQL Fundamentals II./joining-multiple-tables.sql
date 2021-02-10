2beb85f7d5ed17ffa1cfb89cd851073b948db67b
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-10 21:03:30 +0100

    Update joining-multiple-tables.sql

--- SQL Fundamentals II./joining-multiple-tables.sql
+++ SQL Fundamentals II./joining-multiple-tables.sql
@@ -2,13 +2,13 @@
 USE sql_store;
 
 SELECT
-	o.order_id,
+    o.order_id,
     o.order_date,
     c.first_name,
     c.last_name,
     os.name AS status
 FROM orders o
 INNER JOIN customers c
-		  ON o.customer_id = c.customer_id
+	  ON o.customer_id = c.customer_id
 INNER JOIN order_statuses os
-		  ON o.status = os.order_status_id
+	  ON o.status = os.order_status_id
