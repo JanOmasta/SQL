e087f9e6433d54d1abab27a88e83ec49e8152b38
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-10 20:42:44 +0100

    Update self-joins.sql

--- SQL Fundamentals II./self-joins.sql
+++ SQL Fundamentals II./self-joins.sql
@@ -1,11 +1,11 @@
 USE sql_hr;
 
 SELECT
-	e.employee_id,
+    e.employee_id,
     e.first_name,
     e.last_name,
     m.first_name AS manager_first_name,
     m.last_name AS manager_last_name
 FROM employees e								-- To join the table with itself. (The example with CEO.)
 INNER JOIN employees m
-		  ON e.reports_to = m.employee_id
+	  ON e.reports_to = m.employee_id
