705f10a24db2126c62c50df24dff763313bbbdd2
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-11 17:17:21 +0100

    Add files via upload

---
+++ SQL Fundamentals II./using-clause.sql
@@ -0,0 +1,13 @@
+USE sql_store;
+
+SELECT 
+	o.order_id, 
+    c.first_name,
+	sh.name AS shipper
+FROM orders o
+JOIN customers c  
+	-- ON orders.customer_id = customers.customer_id
+	USING (customer_id) 		-- When the column names are exactly the same, we can use the USING clause. For shorter code.
+								-- This code on line 10 is equivalent to the code on line 8.
+LEFT JOIN shippers sh
+	USING (shipper_id)
