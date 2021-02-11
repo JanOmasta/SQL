4cd7a364efa0e16ed813ebd9a9c0c9f067f4759b
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-11 15:48:13 +0100

    Add files via upload

---
+++ SQL Fundamentals II./outer-joins.sql
@@ -0,0 +1,24 @@
+USE sql_store;
+
+-- This is inner join:
+/*
+SELECT 
+	c.customer_id,
+    c.first_name,
+    o.order_id
+FROM customers c
+JOIN orders o
+	ON c.customer_id = o.customer_id
+ORDER BY c.customer_id
+*/
+-- We want to see all customers whether they have order or not. Now we can see only the customers with order.
+
+-- With LEFT OUTER JOIN all the records from the left table (called customers) are returned (whether the condition is true or not). 
+SELECT 
+	c.customer_id,
+    c.first_name,
+    o.order_id
+FROM customers c
+LEFT JOIN orders o							-- This is OUTER JOIN. Could be also RIGHT OUTER JOIN. 
+		 ON c.customer_id = o.customer_id
+ORDER BY c.customer_id
