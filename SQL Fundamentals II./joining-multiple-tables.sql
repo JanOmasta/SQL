e94e604d2ba17e3fe3c976ae0c8f663d047a8abf
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-11 15:12:05 +0100

    Add files via upload

--- SQL Fundamentals II./joining-multiple-tables.sql
+++ SQL Fundamentals II./joining-multiple-tables.sql
@@ -1,14 +1,14 @@
--- How to join 3 tables (and more).
+-- How to join 3 tables (and more) within one database.
 USE sql_store;
 
 SELECT
-    o.order_id,
+	o.order_id,
     o.order_date,
     c.first_name,
     c.last_name,
     os.name AS status
 FROM orders o
 INNER JOIN customers c
-	  ON o.customer_id = c.customer_id
+		  ON o.customer_id = c.customer_id
 INNER JOIN order_statuses os
-	  ON o.status = os.order_status_id
+		  ON o.status = os.order_status_id
