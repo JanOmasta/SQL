aa06a409ceab8594bf58e31f76dcd1d926d07d8f
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-11 17:00:32 +0100

    Add files via upload

---
+++ SQL Fundamentals II./self-outer-join.sql
@@ -0,0 +1,11 @@
+-- To get all employees in this (one) table whether they have manager or not.
+USE sql_hr;
+
+SELECT
+	e.employee_id,
+    e.first_name,
+    e.last_name,
+    m.first_name AS manager
+FROM employees e								
+LEFT JOIN employees m					-- Using LEFT OUTER JOIN
+	ON e.reports_to = m.employee_id
