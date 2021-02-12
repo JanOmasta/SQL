9b393179b07a25fbc5590fe81bf296d0d915ed14
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-12 19:31:25 +0100

    Update inserting-multiple-rows.sql

--- SQL Fundamentals III./inserting-multiple-rows.sql
+++ SQL Fundamentals III./inserting-multiple-rows.sql
@@ -1,10 +1,10 @@
 USE sql_store;
 
 INSERT INTO shippers (
-	name
+    name
     )
 VALUES 
-	('Amazon'),				-- When there are 3 columns, the values are in the shape:  ('Product1', 10, 1.95).
+    ('Amazon'),				-- When there are 3 columns, the values are in the shape:  ('Product1', 10, 1.95).
     ('UPS'),
     ('Fed Ex'),
-	('Post Company ABC')
+    ('Post Company ABC')
