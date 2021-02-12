193f895852b8f2c0d6eef71d951fb713a34b94fb
JanOmasta <76532934+JanOmasta@users.noreply.github.com>
2021-02-12 16:24:43 +0100

    Update natural-joins.sql

--- SQL Fundamentals II./natural-joins.sql
+++ SQL Fundamentals II./natural-joins.sql
@@ -1,8 +1,8 @@
 USE sql_store;
 
 SELECT
-	o.order_id,
+    o.order_id,
     c.first_name
 FROM orders o
-NATURAL JOIN customers c		-- NATURAS JOIN joins 2 tables without defining which columns are the same. 
-								-- However, this fact can cause problems. 
+NATURAL JOIN customers c		-- NATURAL JOIN joins 2 tables without defining which columns are the same so the code is shorter. 
+					-- However, this fact can cause problems. 
